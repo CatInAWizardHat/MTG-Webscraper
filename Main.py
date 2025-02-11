6ac33e1246da26df399c66d12eb2996459287cae
WizardCat <BeigeModel3@proton.me>
2025-02-11 11:22:56 -0800

    updated

--- Main.py
+++ Main.py
@@ -6,13 +6,6 @@ import numpy as np
 
 card_name = input("Card name: ")
 
-
-url = f"https://www.kanatacg.com/products/search?q={card_name}&c=1"
-headers = {"Accept-Language": "en-US, en;q=0.5"}
-results = requests.get(url, headers=headers)
-
-soup = BeautifulSoup(results.text, "html.parser")
-
 # initiate data storage
 prices_nm = []
 prices_vg = []
@@ -20,11 +13,19 @@ prices_ex = []
 category_edition = []
 in_stock = []
 
+try:
+    url = f"https://www.kanatacg.com/products/search?q={card_name}&c=1"
+    headers = {"Accept-Language": "en-US, en;q=0.5"}
+    results = requests.get(url, headers=headers)
 
-card_div = soup.find_all('form', class_='add-to-cart-form')
-
-# our loop through each container
-for container in card_div:
-    print(container)
-    # category/edition
-    # edition = 
+    soup = BeautifulSoup(results.text, "html.parser")
+    card_div = soup.find_all('form', class_='add-to-cart-form')
+    # our loop through each container
+    for container in card_div:
+        print(container)
+        # category/edition
+        # edition =
+except Exception as details:
+    print(details)
+finally:
+    print(f'Thank you.')
